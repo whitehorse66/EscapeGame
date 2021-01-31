56387320a063656e934bdfaec58a14cdabe8b35a
whitehorse66 <mkuwana@keio.jp>
2021-01-31 23:00:17 +0900

    grab

--- Source/Escape/OpneDoor.cpp
+++ Source/Escape/OpneDoor.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "OpneDoor.h"
 #include "GameFramework/Actor.h"
 
@@ -21,7 +23,13 @@ void UOpneDoor::BeginPlay()
 	Super::BeginPlay();
 	InitialYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitialYaw;
-	TargetYaw += InitialYaw;	
+	OpenAngle += InitialYaw;	
+
+	if (!PressurePlate) {
+		UE_LOG(LogTemp, Error, TEXT("%s has the open door component on it, but no pressureplate set"), *GetOwner()->GetName());
+	}
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 
@@ -30,20 +38,34 @@ void UOpneDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	if (PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+	if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens)) {
 		OpenDoor(DeltaTime);
+		DoorLastOpened = GetWorld()->GetTimeSeconds();
+	}
+	else {
+		//DoorCloseDelay–¢–ž‚Ì‚Æ‚«‚Í•Â‚¶‚È‚¢
+		if (GetWorld()->GetTimeSeconds()-DoorLastOpened>DoorCloseDelay) {
+			CloseDoor(DeltaTime);
+		}
 	}
 
-
+	GetWorld()->GetTimeSeconds();
 
 	//UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
 	//UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
-
-
 }
 
 void UOpneDoor::OpenDoor(float DeltaTime) {
-	CurrentYaw = FMath::Lerp(CurrentYaw, TargetYaw, DeltaTime * 0.5f);
+	CurrentYaw = FMath::Lerp(CurrentYaw, OpenAngle, DeltaTime * DoorOpenSpeed);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+
+}
+
+
+void UOpneDoor::CloseDoor(float DeltaTime) {
+	CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * DoorCloseSpeed);
 	FRotator DoorRotation = GetOwner()->GetActorRotation();
 	DoorRotation.Yaw = CurrentYaw;
 	GetOwner()->SetActorRotation(DoorRotation);
--- Source/Escape/OpneDoor.h
+++ Source/Escape/OpneDoor.h
@@ -25,17 +25,32 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void OpenDoor(float DeltaTime);
+	void CloseDoor(float DeltaTime);
+
 
 private:
 	float InitialYaw;
 	float CurrentYaw;
 	
 	UPROPERTY(EditAnywhere)
-	float TargetYaw=90.f;
+	float OpenAngle=90.f;
+
+	float DoorLastOpened = 0.f;
+
+	UPROPERTY(EditAnywhere)
+		float DoorCloseDelay = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+		float DoorOpenSpeed = 0.8f;
+
+	UPROPERTY(EditAnywhere)
+		float DoorCloseSpeed = 2.f;
 
 	UPROPERTY(EditAnywhere)
 		ATriggerVolume* PressurePlate;
 
 	UPROPERTY(EditAnywhere)
 		AActor* ActorThatOpens;
+
+
 };
